a1c5dccb2c3067a3a7d8b7978f39464d71b3c0a6
hognipetersen <hognipetersen@gmail.com>
2014-11-25 17:23:34 +0100

    First commit

---
+++ ClientTEOHHP/src/config/Configurations.java
@@ -0,0 +1,53 @@
+package config;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+// Importing json packaes
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Configurations {
+    // FFkey is used in Encryption.java
+    private String ffcryptkey;
+    
+    public Configurations(){
+    	readFile();
+    }
+
+    //ffcryptkey
+    public String getFfcryptkey() {
+        return ffcryptkey;
+    }
+
+    public void setFfcryptkey(String ffcryptkey) {
+        this.ffcryptkey = ffcryptkey;
+    }
+
+    // Method to read files from jSON file
+    public void readFile() {
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            FileReader json = new FileReader("src/config.json");
+
+            Object obj = jsonParser.parse(json);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // Getting json values for KEY variables
+            setFfcryptkey((String) jsonObject.get("ffcryptkey"));
+
+
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
